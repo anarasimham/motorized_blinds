974b4633b41d04b1fe06d3b997be27234229febc
Ashish Narasimham <ashish.narasimham@gmail.com>
2019-11-03 19:38:37 -0500

    moved models into folder

--- start_send_time.sh
+++ start_send_time.sh
@@ -4,6 +4,7 @@ i=0
 while [ $i -lt 1000 ]
 do
     echo "Trying to start program"
+    #python3 ~/automated_blinds/host_proc/linux_send_time.py 2>&1 | tee ~/send_time.out
     python3 ~/automated_blinds/host_proc/linux_send_time.py
     sleep 5
     ((i=i+1))
